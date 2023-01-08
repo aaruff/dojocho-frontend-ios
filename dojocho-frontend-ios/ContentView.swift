cb6f3581ee0759e474c271b8072fe0c65e05ef67
Anwar A. Ruff <anwarruff@gmail.com>
2023-01-07 21:49:10 -0500

    Initial Commit

---
+++ dojocho-frontend-ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  dojocho-frontend-ios
+//
+//  Created by Anwar Ruff on 1/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dojocho-frontend-ios/dojocho_frontend_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dojocho_frontend_iosApp.swift
+//  dojocho-frontend-ios
+//
+//  Created by Anwar Ruff on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct dojocho_frontend_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
