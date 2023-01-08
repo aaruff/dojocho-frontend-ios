999d7b128c56c34f8c300eedca6bcae0840cf428
Anwar A. Ruff <anwarruff@gmail.com>
2023-01-07 22:04:28 -0500

    Add remote fetching of user data via localhost

--- dojocho-frontend-ios/ContentView.swift
+++ dojocho-frontend-ios/ContentView.swift
@@ -7,16 +7,64 @@
 
 import SwiftUI
 
+struct User: Hashable, Codable {
+    let id: Int
+    let email: String
+    let name: String
+}
+
+class UserCollection: ObservableObject {
+    @Published var users: [User] = []
+    
+    func fetch() {
+        guard let url = URL(string: "http://127.0.0.1:8000/users") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let requestURL = URLRequest(url: url)
+       
+        URLSession.shared.dataTask(with: requestURL) { data, response, error in
+            if let data = data {
+                if let users = try? JSONDecoder().decode([User].self, from: data) {
+                    DispatchQueue.main.async {
+                        self.users = users
+                    }
+                    
+                    return
+                }
+                else {
+                    print("Failed to decode response from data")
+                }
+            }
+            else {
+                print("Fetch failed: \(error?.localizedDescription ?? "Unknown Error")")
+            }
+            print(self.users)
+        }
+        .resume()
+        
+    }
+}
+
+
 struct ContentView: View {
+    @State var email = ""
+    @StateObject var userCollection = UserCollection()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Email", text: $email, onCommit: {})
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Button("Submit"){
+                print(userCollection.users)
+            }
+            .buttonStyle(.bordered)
         }
-        .padding()
+        .onAppear(perform: userCollection.fetch)
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
